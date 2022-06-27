f4329215ce81c72c1ad4395373b575a9b4b8ff7b
Kaylyn Phan <kaylynphan@fb.com>
2022-06-27 10:16:52 -0700

    sign in/sign up + segue to chat vc

---
+++ Parse Chat/ChatViewController.h
@@ -0,0 +1,16 @@
+//
+//  ChatViewController.h
+//  Parse Chat
+//
+//  Created by Kaylyn Phan on 6/27/22.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Parse Chat/LoginViewController.m
+++ Parse Chat/LoginViewController.m
@@ -19,46 +19,13 @@
 
 @implementation LoginViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-- (void)registerUser {
-    // initialize a user object
-    PFUser *newUser = [PFUser user];
-    
-    // set user properties
-    newUser.username = self.usernameField.text;
-    newUser.password = self.passwordField.text;
-    
-    // call sign up function on the object
-    // signUpInBackgroundWithBlock is an async function
-    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
-        if (error != nil) {
-            NSLog(@"Error: %@", error.localizedDescription);
-        } else {
-            NSLog(@"User registered successfully");
-            
-            // manually segue to logged in view
-        }
-    }];
-}
-
-- (void)loginUser {
-    NSString *username = self.usernameField.text;
-    NSString *password = self.passwordField.text;
-    
-    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
-        if (error != nil) {
-            NSLog(@"User log in failed: %@", error.localizedDescription);
-        } else {
-            NSLog(@"User logged in successfully");
-            
-            // display view controller that needs to shown after successful login
-        }
-    }];
-}
 
 /*
 #pragma mark - Navigation
@@ -71,8 +38,83 @@
 */
 
 - (IBAction)loginUser:(id)sender {
+    UIAlertController *nullUsernameAlert = [UIAlertController alertControllerWithTitle:@"Username Required"
+                                                                               message:@"Please enter a username."
+                                                                        preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertController *nullPasswordAlert = [UIAlertController alertControllerWithTitle:@"Password Required"
+                                                                               message:@"Please enter a password."
+                                                                        preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"OK"
+                                                        style:UIAlertActionStyleCancel
+                                                      handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle cancel response here. Doing nothing will dismiss the view.
+                                                      }];
+    // add the cancel action to the alertController
+    [nullUsernameAlert addAction:cancelAction];
+    [nullPasswordAlert addAction:cancelAction];
+    
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    if ([self.usernameField.text isEqual:@""]) {
+        [self presentViewController:nullUsernameAlert animated:YES completion:^{
+        }];
+    } else if ([self.passwordField.text isEqual:@""]) {
+        [self presentViewController:nullPasswordAlert animated:YES completion:^{
+        }];
+    } else {
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+            if (error != nil) {
+                NSLog(@"User log in failed: %@", error.localizedDescription);
+            } else {
+                NSLog(@"User logged in successfully");
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            }
+        }];
+    }
 }
 
 - (IBAction)registerUser:(id)sender {
+    UIAlertController *nullUsernameAlert = [UIAlertController alertControllerWithTitle:@"Username Required"
+                                                                               message:@"Please enter a username."
+                                                                        preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertController *nullPasswordAlert = [UIAlertController alertControllerWithTitle:@"Password Required"
+                                                                               message:@"Please enter a password."
+                                                                        preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"OK"
+                                                        style:UIAlertActionStyleCancel
+                                                      handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle cancel response here. Doing nothing will dismiss the view.
+                                                      }];
+    // add the cancel action to the alertController
+    [nullUsernameAlert addAction:cancelAction];
+    [nullPasswordAlert addAction:cancelAction];
+    
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    // create alert controllers
+    
+    // call sign up function on the object
+    // signUpInBackgroundWithBlock is an async function
+    if ([self.usernameField.text isEqual:@""]) {
+        [self presentViewController:nullUsernameAlert animated:YES completion:^{
+        }];
+    } else if ([self.passwordField.text isEqual:@""]) {
+        [self presentViewController:nullPasswordAlert animated:YES completion:^{
+        }];
+    } else {
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+            if (error != nil) {
+                NSLog(@"Error: %@", error.localizedDescription);
+            } else {
+                NSLog(@"User registered successfully");
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            }
+        }];
+    }
 }
 @end
